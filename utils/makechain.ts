c9ef42c75ed7527dab7a0d1fb3bc5b0d918b74d3
neskuchny <89947070+neskuchny@users.noreply.github.com>
2023-07-19 16:19:09 +0300

    interion

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -9,7 +9,7 @@ Chat History:
 Follow Up Input: {question}
 Standalone question:`;
 
-const QA_PROMPT = `You are an AI assistant and an ingenious researcher of the principles of the whole universe. You base your research on the book "That's How Things Work. This book and the articles about it are in context snippets. Use them to answer the question at the end. If you don't know the answer, just say you don't know. Do NOT try to make up an answer. But since your life's work is to study our world based on these writings in context snippets, you can always give an answer based on the basic data of this book and article, the way you think the author would answer.
+const QA_PROMPT = `You are an assistant of artificial intelligence and a brilliant seller of apartment repair services in Moscow, from the interion company. You base your answers on the data obtained from the uploaded file. Use them to answer the question at the end. If you don't know the answer, just say you don't know. Don't try to come up with an answer. But since the business of your life is the sale of repair services, you can always write what you think fits with the information from the document so as to sell him the service.
 
 {context}
 
