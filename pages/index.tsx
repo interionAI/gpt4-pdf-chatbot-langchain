3ca4aa248c0e400b1de3b55f152e1f47a27b79e8
neskuchny <89947070+neskuchny@users.noreply.github.com>
2023-07-19 16:21:48 +0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -125,7 +125,7 @@ export default function Home() {
       <Layout>
         <div className="mx-auto flex flex-col gap-4">
           <h1 className="text-2xl font-bold leading-[1.1] tracking-tighter text-center">
-            Чат о `Теорие самоподобия`, по книге `Вот как всё устроено`. Калабухов А.С
+            Чат-бот компании interion
           </h1>
           <main className={styles.main}>
             <div className={styles.cloud}>
@@ -224,7 +224,7 @@ export default function Home() {
                     placeholder={
                       loading
                         ? 'Waiting for response...'
-                        : 'В чем основная идея книги?'
+                        : 'какие у вас основные преимущества?'
                     }
                     value={query}
                     onChange={(e) => setQuery(e.target.value)}
